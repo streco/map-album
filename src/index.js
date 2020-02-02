fe0f8e99391ae7fb7356d6ed3fe90e066bfc8f02
Steven Greco <streco@Stevens-MacBook-Pro.local>
2020-02-01 20:57:05 -0800

    Added Mapbox Sample

--- src/index.js
+++ src/index.js
@@ -3,8 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import mapboxgl from 'mapbox-gl';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3RyZWNvIiwiYSI6ImNrNjRqaGJqcTEwbXczanFvdm1kbXB4NW0ifQ.KqTVv6xamprq1sUD2jWAZQ';
+
+class Application extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            lng: 5,
+            lat: 34,
+            zoom: 2
+        };
+    }
+
+    componentDidMount() {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div ref={el => this.mapContainer = el} className="mapContainer" />
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Application />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
