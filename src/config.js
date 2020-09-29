b52b998a28263cd0a9e510096456bbe392f61504
Steven Greco <streco@Stevens-MacBook-Pro.local>
2020-09-28 21:27:04 -0700

    S3 Example

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const CLIENT_ID = ''
-const CLIENT_SECRET = '';
+const CLIENT_ID = '892434940331-plk29rdeeq7f9mvms4gv14eeeapth29l.apps.googleusercontent.com'
+const CLIENT_SECRET = '7XG-y_nrR9h2BsYGdPtWBtqv';
 const REDIRECT_URL = 'http://localhost:3000/auth/google/callback'
-const MAPBOX_ACCESS_TOKEN = '';
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoic3RyZWNvIiwiYSI6ImNrNjRqaGJqcTEwbXczanFvdm1kbXB4NW0ifQ.KqTVv6xamprq1sUD2jWAZQ';
 
 export { CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, MAPBOX_ACCESS_TOKEN};
--- src/mapAlbum.js
+++ src/mapAlbum.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import { MAPBOX_ACCESS_TOKEN } from './config';
-import GooglePhotosProvider from './googlePhotosProvider';
+import AWS from 'aws-sdk';
 
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
+
+/*
+AWS.config.region = 'us-east-2'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-2:39c898ec-b8df-417b-9df8-681588cbffb8',
+});
+*/
 
 class MapAlbum extends React.Component {
     constructor(props) {
@@ -23,12 +30,46 @@ class MapAlbum extends React.Component {
             zoom: this.state.zoom
         });
 
+        // **DO THIS**:
+        //   Replace BUCKET_NAME with the bucket name.
+        //
+        var albumBucketName = 'mapalbum';
+
+        // **DO THIS**:
+        //   Replace this block of code with the sample code located at:
+        //   Cognito -- Manage Identity Pools -- [identity_pool_name] -- Sample Code -- JavaScript
+        //
+        // Initialize the Amazon Cognito credentials provider
+        AWS.config.region = 'us-east-2'; // Region
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: 'us-east-2:39c898ec-b8df-417b-9df8-681588cbffb8',
+        });
+
+        // Create a new service object
+        var s3 = new AWS.S3({
+            apiVersion: '2006-03-01',
+            params: {Bucket: albumBucketName}
+        });
+
+        s3.listObjects({Delimiter: '/'}, function(err, data){
+            if(err){
+                console.log('unable to retrieve objects');
+            }
+            else{
+                console.log(data);
+                for(let photo of data.Contents){
+                    console.log(`${this.request.httpRequest.endpoint.href}mapalbum/${photo.Key}`);
+                }
+            }
+        });
+
     }
+
+   
 
     render() {
         return (
             <div>
-                <GooglePhotosProvider />
                 <div ref={el => this.mapContainer = el} className="mapContainer" />
             </div>
         )
